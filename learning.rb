99adabf350f698a82b2150d3bc906d8526525756
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-09-25 11:41:52 -0500

    Conversiones: Método gets usando chomp
    'chomp' filtra los saltos de línea, retornos de carro que se producen en las salidas (puts)

--- learning.rb
+++ learning.rb
@@ -1,5 +1,7 @@
-# Conversiones: Los Métodos gets
+# Conversiones: Los Métodos gets y chomp
 puts 'Hola, ¿Cuál es tu nombre?';
-name = gets;
+name = gets .chomp;
 puts '¿Tu nombre es ' + name + '?  ¡Es un nombre adorable!';
 puts 'Encantado de conocerte, ' + name + '. :)';
+# --- Cantidad de letras en el nombre ---
+puts '¿Sabías que hay ' + name .length .to_s + ' caracteres en tu nombre, ' + name + '?'
