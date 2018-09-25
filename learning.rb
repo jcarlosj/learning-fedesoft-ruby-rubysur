95abdd003d96f3005ba738067919d16cd5db3b1b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-09-25 10:13:12 -0500

    Números: Los chocolates que comeré en la vida

--- learning.rb
+++ learning.rb
@@ -1,12 +1,3 @@
-puts '¿Cuál es tu año de nacimiento?'
-anio_nacimiento = gets .chomp   # Obtenemos el valor desde el prompt
-
-def edad( birth_day )
-    tiempo_actual = Time .now
-    año_actual = tiempo_actual .strftime( '%Y' ) .to_i
-    edad = año_actual - birth_day .to_i
-    return edad
-end
-
-puts '¿Cuántos segundos de edad tienes?'
-puts ( edad( anio_nacimiento ) * 365 * 24 * 60 * 60 ) .to_s + ' segundos de vida';
+puts '¿Cuántos chocolates esperas comer en tu vida?'
+cantidad_chocolates = gets .chomp   # Obtenemos el valor desde el prompt
+puts cantidad_chocolates + ' deliciosos chocolates rellenos en mi vida'
