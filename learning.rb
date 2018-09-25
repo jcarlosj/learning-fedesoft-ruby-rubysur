77c56cdf31932afa79ca033e0b3f3ad13a6760fd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-09-25 10:21:00 -0500

    Números: Edad de segundos a años

--- learning.rb
+++ learning.rb
@@ -1,3 +1,4 @@
-puts '¿Cuántos chocolates esperas comer en tu vida?'
-cantidad_chocolates = gets .chomp   # Obtenemos el valor desde el prompt
-puts cantidad_chocolates + ' deliciosos chocolates rellenos en mi vida'
+puts 'Si tengo 1031 millones de segundos de edad ¿Cuántos años tengo?';
+segundos_edad = 1031000000;
+anos = segundos_edad / 60 / 60 / 24 / 365;
+puts 'Tengo ' + anos .to_s + ' años'     ;
