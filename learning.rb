eb0fe7de78444eae6dc36395ed0561feb8685d63
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-09-25 10:03:17 -0500

    Números: Segundos de edad (pidiendo año nacimiento)

--- learning.rb
+++ learning.rb
@@ -1,12 +1,12 @@
-puts '¿Cuántos segundos de edad tienes?'
+puts '¿Cuál es tu año de nacimiento?'
+anio_nacimiento = gets .chomp   # Obtenemos el valor desde el prompt
 
-anio_nacimiento = 1977
-
-def edad( anio_nacimiento )
+def edad( birth_day )
     tiempo_actual = Time .now
     año_actual = tiempo_actual .strftime( '%Y' ) .to_i
-    edad = año_actual - anio_nacimiento
-    return edad .to_i
+    edad = año_actual - birth_day .to_i
+    return edad
 end
 
+puts '¿Cuántos segundos de edad tienes?'
 puts ( edad( anio_nacimiento ) * 365 * 24 * 60 * 60 ) .to_s + ' segundos de vida';
