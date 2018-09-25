dc2e3159ebbf5d837565b184167a46c895f8c658
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-09-25 11:23:19 -0500

    Métodos puts/gets (entrada/salida) en Ruby

--- learning.rb
+++ learning.rb
@@ -1,17 +1,2 @@
-# Conversiones
-var1 = 2
-var2 = '5'
-
-puts var1 .to_s + var2
-# ----------------
-# to_i (parsea a entero)
-# to_s (parsea a cadena)
-# to_f (parsea a decimal)
-puts '15' .to_f
-puts '99.999' .to_f
-puts '99.999' .to_i
-puts '5 es mi número favorito.' .to_i
-puts '¿Quién pregunta acerca de 5 o lo que sea?' .to_i
-puts 'Tu mamá hizo.' .to_f
-puts 'fibroso' .to_s
-puts 3 .to_i
+# Conversiones: Los Métodos gets y chomp
+puts gets
