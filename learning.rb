551600247ca1cbba127a2b004731df05fe4a08ea
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-09-25 09:48:31 -0500

    Números: Segundos de edad

--- learning.rb
+++ learning.rb
@@ -1,2 +1,12 @@
-puts '¿Cuántos minutos hay en una década?'
-puts 'Rta: ' + (10 * 365 * 24 * 60) .to_s + ' minutos'
+puts '¿Cuántos segundos de edad tienes?'
+
+anio_nacimiento = 1977
+
+def edad( anio_nacimiento )
+    tiempo_actual = Time .now
+    año_actual = tiempo_actual .strftime( '%Y' ) .to_i
+    edad = año_actual - anio_nacimiento
+    return edad .to_i
+end
+
+puts ( edad( anio_nacimiento ) * 365 * 24 * 60 * 60 ) .to_s + ' segundos de vida';
